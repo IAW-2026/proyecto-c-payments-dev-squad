071f3025e970cac7bdebf55d38688bdb64c3c0d9
AngelinaFB <angelinafernandezbazan@gmail.com>
2026-05-11 10:30:49 -0300

    Fix: db, next.config y next-env.d

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['@prisma/client'],
+  serverExternalPackages: ['@prisma/client', '@prisma/adapter-neon'],
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
   },
